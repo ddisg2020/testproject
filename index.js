5b1fbbe50c30691c1cb868ca0bf64c49f6abf227
ddisg2020 <96202902+ddisg2020@users.noreply.github.com>
2022-04-17 12:01:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,25 +4,19 @@ const maxCitiesQuantity = 3;
 
 const chooseOptimalDistance = (t, k, Is) => {
     if (Is.length <= 1 || k <= 0) return null;
-    let combinations = [];
+
     let countsList = [];
     let itemCount = 0;
     let letLen = Math.pow(k, Is.length);
         for (var i = 0; i < letLen ; i++){
-            let resultItem = [];
+            
             for (let j=0; j < Is.length; j++) {
                 if (( i & Math.pow(2, j) )){
-                    resultItem.push(Is[j]);
+                   
                     itemCount += Is[j];
                 }
             }
-            if (resultItem != [] 
-                && resultItem.length == k 
-                && itemCount < t
-                ){
-                 combinations.push(resultItem)
-                 countsList.push(itemCount);
-            }
+            if ( itemCount < t ) countsList.push(itemCount);
         }
 
     const max = Math.max(...countsList);
